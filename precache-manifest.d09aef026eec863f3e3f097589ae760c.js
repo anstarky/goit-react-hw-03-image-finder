4ae3679b844ca03726f17451313e72c5e0f6e416
“anstarky” <anstarky@gmail.com>
2019-11-25 12:46:35 +0200

    Updates

---
+++ precache-manifest.d09aef026eec863f3e3f097589ae760c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7cba177ff5b228963c03fea12bdcbd3",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "725ce323f4aa413736f5",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "903a154a6ac9b491ac9b",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.3ab82d11.chunk.css"
+  },
+  {
+    "revision": "725ce323f4aa413736f5",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3ba72e05.chunk.js"
+  },
+  {
+    "revision": "903a154a6ac9b491ac9b",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.46d5e2f6.chunk.js"
+  },
+  {
+    "revision": "2b8029fb33bffe02ce95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.c3246142.js"
+  }
+]);
